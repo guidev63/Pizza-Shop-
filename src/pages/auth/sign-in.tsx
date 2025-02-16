e5b563411bba1c28ad41f3f581556b43d48de813
Guilherme Augusto <guilhermedev23@gmail.com>
2025-02-16 20:33:22 -0300

    ok

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,27 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import {Helmet} from "react-helmet-async"
 
 
 export function SignIn() {
-    return( <>
+    return(
+       <>
      <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="w-[350px] flex flex-col justify-center gap-6">
+          <div className=" flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
+             <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro!</p>
+          </div>
+         <form className="space-y-4">
+          <div className="space-y-2">
+          <Label htmlFor="email"> Seu e-mail</Label>
+          <Input id="email" type="email"/>
+          </div>
+          <button type="submit" className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700 transition w-full">Acessar Painel</button>
+         </form>
+        </div>
+      </div>
       </>
     )
 }
