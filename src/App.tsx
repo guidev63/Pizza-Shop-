58e126b1c146fbb11deb0c6b9306803dc2f1d8ad
Guilherme Augusto <guilhermedev23@gmail.com>
2025-02-21 19:50:52 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import {Helmet, HelmetProvider} from 'react-helmet-async'
-import { router } from './routes'
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { router } from "./routes";
+import { Toaster } from "sonner"; // Importação do Toaster
+
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet  titleTemplate="%s | pizza shop"/>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      
+      {/* Componente Toaster para exibir notificações */}
+      <Toaster richColors position="bottom-right" />
+
+      {/* Provedor de rotas */}
       <RouterProvider router={router} />
     </HelmetProvider>
-  )
+  );
 }
-
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -5,26 +5,25 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { toast } from "sonner";
 import { Toaster } from "sonner";
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+
 
 // Definição do schema do formulário com Zod
-const signInForm = z.object({
+const signUpForm = z.object({
   email: z.string().email(),
 });
 
 // Tipo inferido a partir do schema
-type SignInForm = z.infer<typeof signInForm>;
+type SignUpForm = z.infer<typeof signUpForm>;
 
-export function SignIn() {
+export function SignUp() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>();
+  } = useForm<SignUpForm>();
 
   // Função para lidar com o envio do formulário
-  async function handleSignIn(data: SignInForm) {
+  async function handleSignUp(data: SignUpForm) {
     try {
       console.log(data);
 
@@ -35,7 +34,7 @@ export function SignIn() {
       toast.success("Enviamos um link de autenticação para seu e-mail!", {
         action: {
           label: "Reenviar",
-          onClick: () => handleSignIn(data), // Reenvia o link ao clicar no botão
+          onClick: () => handleSignUp(data), // Reenvia o link ao clicar no botão
         },
         duration: 4000, // Tempo de exibição do toast
         position: "bottom-right", // Posicionamento do toast
@@ -49,27 +48,20 @@ export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-
       {/* Componente para exibir toasts */}
       <Toaster richColors position="bottom-right" />
-
       <div className="p-8">
-      <Button asChild className="absolute">
-      <Link
-        to="/sign-up" className="">
-          Novo estabelecimento
-       </Link> 
-      </Button>
+     
         <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
+            <h1 className="text-2xl font-semibold tracking-tight">Criar Conta grátis</h1>
             <p className="text-sm text-muted-foreground">
-              Acompanhe suas vendas pelo painel do parceiro!
+              seja um parceiro e comece suas vendas! 
             </p>
           </div>
 
           {/* Formulário de login */}
-          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input
@@ -91,7 +83,7 @@ export function SignIn() {
                   : "bg-red-600 hover:bg-red-700 cursor-pointer"
                 }`}
             >
-              {isSubmitting ? "Aguarde..." : "Acessar Painel"}
+              {isSubmitting ? "Aguarde..." : "Finalizar Cadastro"}
             </button>
           </form>
         </div>
