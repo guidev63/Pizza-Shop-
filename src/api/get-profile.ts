0ccf2fd60c034e71775c203e458dcb7dc891bac2
Guilherme Augusto <guilhermedev23@gmail.com>
2025-03-07 21:59:39 -0300

    ok

---
+++ src/api/get-profile.ts
@@ -0,0 +1,18 @@
+import { api } from "@/lib/axios";
+
+interface GetProfileResponse {
+  id: string
+  name: string
+  email: string
+  phone: string
+  role: 'manager' | 'customer'
+  createdAt: Date | null
+  updateAt: Date | null
+}
+
+export async function getProfile() {
+  const response = await api.get('/me')
+
+
+  return response.data
+}
