549d45aee926152aa5796f7a40eaaf124a208abc
Guilherme Augusto <guilhermedev23@gmail.com>
2025-03-12 21:29:12 -0300

    ok

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,9 +1,50 @@
-import { DialogContent } from "./ui/dialog";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
+import { Button } from "./ui/button";
+import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+import { useQuery } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
 
-export function StoreProfileDialog () {
-  return(
+const storeProfile
+
+export function StoreProfileDialog() {
+  const { data: managedrestaurant, } = useQuery({
+    queryKey: ['managedrestaurant'],
+    queryFn: getManagedRestaurant,
+  })
+  const { } = useForm()
+  return (
     <DialogContent>
-      teste
+      <DialogHeader>
+        <DialogTitle>Perfil da Loja</DialogTitle>
+        <DialogDescription>
+          Atualize as Informações do seu Estabelecimento visíveis ao seu Cliente
+        </DialogDescription>
+      </DialogHeader>
+
+      <form>
+        <div className="space-y-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">Nome</Label>
+            <Input className="col-span-3" id="name" />
+          </div>
+
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">Descrição</Label>
+            <Textarea className="col-span-3" id="description" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button variant="ghost" type="button">
+            Cancelar
+          </Button>
+          <Button type="submit" variant="success">
+            Salvar
+          </Button>
+        </DialogFooter>
+      </form>
     </DialogContent>
-  )
+  );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,9 +1,8 @@
 import * as React from "react"
-
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from '@/lib/utils'; // Certifique-se de que o caminho está correto
+import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
@@ -14,6 +13,8 @@ const buttonVariants = cva(
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        success:
+          "bg-emerald-500 text-white  hover:bg-emerald-500/90 dark:bg-emerald-600 dark:hover:bg-emerald-600/80",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
@@ -24,7 +25,6 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
-        xs:"h-8 px-2.5",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
       },
@@ -54,7 +54,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     )
   }
 )
-
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
