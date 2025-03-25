ef39e996f1e42857c055a2ab0e2c406bc42fa659
Guilherme Augusto <guilhermedev23@gmail.com>
2025-03-24 23:20:40 -0300

    ok

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -1,1 +1,16 @@
 type  OderStatus = 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+
+interface OrderStatusProps {
+  status: OrderStatus
+}
+const orderStatusMap:Record<OrderStatusProps,string>  = {
+pending:'Pendente',
+canceled:'Canceledo',
+delivered:'Entregue',
+processing:'Em preparo',
+}
+export function OrderStatus(){
+   return (
+    <div></div>
+   )
+}
