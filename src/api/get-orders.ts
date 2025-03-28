f674da30d49a7826eb3a115841c8af98745fe356
Guilherme Augusto <guilhermedev23@gmail.com>
2025-03-27 22:04:33 -0300

    ok

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,5 +1,7 @@
 import { api } from "@/lib/axios";
-
+export interface GetOrdersQuery{
+  pageIndex?: number | null 
+}
 export interface GetOrdersResponse {
   orders: {
     ordersId: string
@@ -15,10 +17,10 @@ export interface GetOrdersResponse {
 
   }
 }
-export async function getOrders() {
+export async function getOrders({pageIndex}: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
-      pageIndex: 0,
+      pageIndex,
     },
   })
 
